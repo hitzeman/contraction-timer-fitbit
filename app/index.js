7f8c9a0bd031b6d4e16d4eb2d169d7ae81c594ea
Todd Hitzeman <hitzeman.todd@gmail.com>
2018-06-08 21:26:41 -0700

    added 00:00 format and centered textarea. added panorama view to have two views

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ import document from "document";
 
 /* Variables */
 let active = false;
-let seconds = 0;
-let contractions = [];
+const seconds = 0;
+const contractions = [];
 let contractionStart, contractionStop;
 
 /* Element References */
@@ -14,30 +14,58 @@ let btnBRIcon = document.getElementById("combo-button-icon");
 let btnBrIconPress = document.getElementById("combo-button-icon-press");
 
 /* Event Handlers */
-btnBR.onactivate = function(e) {
+btnBR.onactivate = function() {
   if (active) {
-    active = false;
-    contractionStop = new Date();
+    activate(false);
+    timeContraction(false);
     
-    btnBRIcon.image = "icons/btn_combo_play_p.png";  
-    btnBrIconPress.image = "icons/btn_combo_play_press_p.png";
-    
-    // Stop counting
-    clock.granularity = "off";
-    
-    contractions.push({start: startTime, stop: stopTime, seconds: seconds});
+    contractions.push({start: contractionStart, stop: contractionStop, seconds: seconds});
+    console.log(JSON.stringify(contractions));
+
+    seconds = 0;
+    timer.textContent = formatTime(0);
   } else {
-    active = true;
-    contractionStart = new Date();
-    
-    btnBRIcon.image = "icons/btn_combo_pause_p.png";  
-    btnBrIconPress.image = "icons/btn_combo_pause_press_p.png";
-    
-    // Start counting
-    clock.granularity = "seconds";
-    clock.ontick = (e) => {
-      seconds++;
-      timer.textContent = seconds;
-    }
+    activate(true);
+    timeContraction(true);
   }
 }
+
+function activate(activeFlag) {
+  active = !active;
+  if (activeFlag) {
+    contractionStart = new Date();
+  } else {
+    contractionStop = new Date();
+  }
+  
+  const buttonMode = active ? 'pause' : 'play';
+  
+  btnBRIcon.image = `icons/btn_combo_${buttonMode}_p.png`;  
+  btnBrIconPress.image = `icons/btn_combo_${buttonMode}_press_p.png`;
+}
+
+function timeContraction(timeFlag) {
+  if (timeFlag) {
+    clock.granularity = "seconds";
+    clock.ontick = () => {
+      seconds++;
+      timer.textContent = formatTime(seconds);
+    }
+  } else {
+    // Stop counting by no longer emitting event
+    clock.granularity = "off";
+  }
+}
+
+function formatTime(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(14, 5);
+}
+
+
+
+
+
+
+
+
+
